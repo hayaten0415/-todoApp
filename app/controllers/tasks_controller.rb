1f0dcb139d99d8d61ef0ecffda10a25547cff928
Hayashi Hironori <hayaten415@gamil.com>
2018-09-19 23:41:31 +0900

    finish part2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,31 @@ class TasksController < ApplicationController
         end
     end
 
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+
+        if @task.update(task_params)
+            redirect_to tasks_path
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+
+        @task.destroy
+        redirect_to tasks_path
+    end
+
     private
     def task_params
         params.require(:task).permit(:title, :description)
